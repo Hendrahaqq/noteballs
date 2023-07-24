239adc5e070323b9781f400bd653edc7086164c1
Muhammad Hendra Nailul Haqq <hendrahaqq@gmail.com>
2023-07-24 22:05:49 +0700

    initial push

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+
+  <NavBar/>
+  
+  <RouterView />
+</template>
+<script setup>
+
+import NavBar from './components/Layout/NavBar.vue';
+</script>
+<style>
+  @import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import ViewNotes from '../views/ViewNotes.vue'
+import ViewStats from '../views/ViewStats.vue'
+const routes = [
+  {
+    path: '/',
+    name: 'notes',
+    component: ViewNotes
+  },
+  {
+    path: '/stats',
+    name: 'stats',
+    component: ViewStats
+  },
+]
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes
+})
+
+export default router
